94bd128f3859a3a5a12d384896fe83782c93b722
firstdreamding <huaxu3721@yahoo.com>
2020-08-28 23:27:30 -0500

    I finished stuff: Schedule started

---
+++ schedule_list.js
@@ -0,0 +1,32 @@
+myFunction([
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+},
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+},
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+},
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+},
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+},
+{
+"tag": "gold",
+"date": "8/28/2020",
+"team1": "g2 vs fnc"
+}
+])
